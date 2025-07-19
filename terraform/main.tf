f82ace08733f43784a2cfb29f5c1dd98c3960acd
justrunme <justrunme@gmail.com>
2025-07-19 14:42:45 +0200

    Fix nginx PID file issue: run as root to access /var/run

--- terraform/main.tf
+++ terraform/main.tf
@@ -665,17 +665,17 @@ resource "kubernetes_deployment" "test_app" {
           security_context {
             allow_privilege_escalation = false
             read_only_root_filesystem  = false
-            run_as_non_root            = true   # Запускаем как непривилегированный пользователь
-            run_as_user                = 101    # nginx user в этом образе
+            run_as_non_root            = false  # Разрешаем запуск от root для nginx
+            run_as_user                = 0      # Запускаем от root
             capabilities {
               drop = ["ALL"]
             }
           }
         }
 
-        # Все смонтированные тома получат группу 101 (nginx) и будут записываемы
+        # Запускаем контейнер от root для доступа к /var/run
         security_context {
-          fs_group = 101
+          run_as_user = 0
         }
       }
     }
